5c683bf6d62b8bd0dded1802a4c3efd581f404cc
Hallo0026 <vinicius_santossouza@outlook.com>
2023-11-25 19:36:00 -0300

    Agrupamento de rotas

--- Code/app_super_gestao/routes/web.php
+++ Code/app_super_gestao/routes/web.php
@@ -20,10 +20,16 @@ use Illuminate\Support\Facades\Route;
 // Chamando a rota utilizando controladores
 
 Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal']);
-
 Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class, 'sobreNos']);
-
 Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato']);
+Route::get('/login', function(){return 'Login';});
+
+
+Route::prefix('/app')->group(function() {
+    Route::get('/clientes', function(){ return 'Clientes'; });
+    Route::get('/fornecedores', function(){ return 'Fornecedores'; });
+    Route::get('/produtos', function(){ return 'Produtos'; });
+});
 
 
 
