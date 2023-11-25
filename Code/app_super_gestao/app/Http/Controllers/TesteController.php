7b3a69f994c34cdd0fde71b94ce01825f4917a50
Hallo0026 <vinicius_santossouza@outlook.com>
2023-11-25 20:22:46 -0300

    Formas de passar parâmetros para a view

--- Code/app_super_gestao/app/Http/Controllers/TesteController.php
+++ Code/app_super_gestao/app/Http/Controllers/TesteController.php
@@ -7,6 +7,24 @@ use Illuminate\Http\Request;
 class TesteController extends Controller
 {
     public function teste(int $p1, int $p2) {
-        echo "A soma de $p1 + $p2 é: " . $p1 + $p2;
+
+        // Passando os parâmetros utilizando um array associativo
+
+        //echo "A soma de $p1 + $p2 é: " . $p1 + $p2;
+        /*return view('site.teste', [
+            'p1' => $p1,
+            'p2' => $p2
+        ]);*/
+
+
+        // Passando os parâmetros utilizando o compact()
+
+        /*return view('site.teste', compact('p1', 'p2'));*/
+
+        // Passando os parâmetros utilizando o with()
+
+        return view('site.teste')->with('p1', $p1)->with('p2', $p2);
+        // Primeiro parâmetro é o nome que a variável terá na view, e o segundo é o seu valor
+
     }
 }
