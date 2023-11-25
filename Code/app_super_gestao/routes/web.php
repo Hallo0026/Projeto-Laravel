27c9e94488c0511956e4bcb40a9953bd97d42b17
Hallo0026 <vinicius_santossouza@outlook.com>
2023-11-25 16:37:38 -0300

    Explicacao basica controllers

--- Code/app_super_gestao/routes/web.php
+++ Code/app_super_gestao/routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about-us', function () {
+    return 'About us';
+});
+
+Route::get('/contact', function () {
+    return 'Contact';
+});
+
+//Route::get($uri, $callback);
