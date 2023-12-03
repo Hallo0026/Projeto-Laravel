863e8d8adc3ec5d3cf96b1079fed814761688a78
Hallo0026 <vinicius_santossouza@outlook.com>
2023-12-03 18:21:30 -0300

    Laços de repetição Blade

--- Code/app_super_gestao/app/Http/Controllers/FornecedorController.php
+++ Code/app_super_gestao/app/Http/Controllers/FornecedorController.php
@@ -11,15 +11,36 @@ class FornecedorController extends Controller
             0 => [
                 'nome' => 'Fornecedor 1',
                 'status' => 'N',
-                'cnpj' => '00.000.000/000-00'
+                'cnpj' => '00.000.000/000-00',
+                'ddd' => '11', // São Paulo (SP)
+                'telefone' => '00000-0000'
             ],
 
             1 => [
                 'nome' => 'Fornecedor 2',
                 'status' => 'S',
+                'ddd' => '85', // Fortaleza (CE)
+                'telefone' => '00000-0000'
+            ],
+
+            2 => [
+                'nome' => 'Fornecedor 3',
+                'status' => 'S',
+                'ddd' => '32', // Juiz de Fora (MG)
+                'telefone' => '00000-0000'
             ]
 
         ];
+
+        /*
+        // Lógica operador ternário: {condicao} ? {se verdadeiro} : {se falso}
+        //                           {condicao} ? {se verdadeiro} : ( {condicao} ? {se verdadeiro} : se falso )
+
+        $msg = isset($fornecedores[1]['cnpj']) ? 'CNPJ informado' : 'CNPJ não informado';
+        echo $msg;
+        */
+
         return view('app.fornecedor.index', compact('fornecedores'));
+
     }
 }
--- Code/app_super_gestao/resources/views/app/fornecedor/index.blade.php
+++ Code/app_super_gestao/resources/views/app/fornecedor/index.blade.php
@@ -26,33 +26,85 @@
 
 {{-- @dd($foo) -> Imprime um array --}}
 @isset($fornecedores)
-    Fornecedor: {{ $fornecedores[1]['nome'] }}
-    <br>
-    Status: {{ $fornecedores[1]['status'] }}
-    <br>
-    @isset($fornecedores[1]['cnpj'])
-        CNPJ: {{ $fornecedores[1]['cnpj'] }}
-    @endisset
+
+    @php /*
+
+    @for ($i = 0; isset($fornecedores[$i]); $i++)
+
+        Fornecedor: {{ $fornecedores[$i]['nome'] ?? '' }}
+        <br>
+
+        Status: {{ $fornecedores[$i]['status'] }}
+        <br>
+
+        @isset($fornecedores[$i]['cnpj'])
+
+            CNPJ: {{ $fornecedores[$i]['cnpj'] }}
+            @empty($fornecedores[$i]['cnpj'])
+                - Vazio
+            @endempty
+
+        @endisset
+
+        Telefone: ({{$fornecedores[$i]['ddd'] ?? '' }}) {{ $fornecedores[$i]['telefone'] ?? '' }}
+
+        @if ($fornecedores[$i]['status'] == 'S')
+            <br> Fornecedor ativo
+        @endif
+
+        @unless($fornecedores[$i]['status'] == 'S') <!-- Executa se o retorno da condição for false -->
+            <br> Fornecedor inativo
+        @endunless
+
+        <hr>
+
+    @endfor
+
+    */ @endphp
+
+
+    @foreach ($fornecedores as $indice => $fornecedor)
+
+        Fornecedor: {{ $fornecedor['nome'] ?? '' }}
+        <br>
+
+        Status: {{ $fornecedor['status'] }}
+        <br>
+
+        @isset($fornecedor['cnpj'])
+
+            CNPJ: {{ $fornecedor['cnpj'] }}
+            @empty($fornecedor['cnpj'])
+                - Vazio
+            @endempty
+
+        @endisset
+
+        Telefone: ({{$fornecedor['ddd'] ?? '' }}) {{ $fornecedor['telefone'] ?? '' }}
+
+        @if ($fornecedor['status'] == 'S')
+            <br> Fornecedor ativo
+        @endif
+
+        @unless($fornecedor['status'] == 'S') <!-- Executa se o retorno da condição for false -->
+            <br> Fornecedor inativo
+        @endunless
+
+        <hr>
+
+    @endforeach
+
 @endisset
 
-@if ($fornecedores[0]['status'] == 'S')
-    Fornecedor ativo
-@endif
-
-<br>
-
-@unless($fornecedores[0]['status'] == 'S') <!-- Executa se o retorno da condição for false -->
-    Fornecedor inativo
-@endunless
 
 
 @if(count($fornecedores) < 10)
 
-        <h3>Existem alguns fornecedores cadastrados.</h3>
+    <h3>Existem alguns fornecedores cadastrados.</h3>
 
 @elseif(count($fornecedores) >= 10)
 
-        <h3>Existem vários fornecedores cadastrados.</h3>
+    <h3>Existem vários fornecedores cadastrados.</h3>
 
 @else
 
