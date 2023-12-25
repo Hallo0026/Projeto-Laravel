6daebdfdc74a0abc31495f68bb23a41ab69a9ce9
Hallo0026 <vinicius_santossouza@outlook.com>
2023-12-25 15:04:55 -0300

    Form insert

--- Code/app_super_gestao/app/Models/SiteContato.php
+++ Code/app_super_gestao/app/Models/SiteContato.php
@@ -15,6 +15,7 @@ class SiteContato extends Model
 {
     use HasFactory;
 
-    
+    protected $table = 'site_contatos'; // Definindo o nome da tabela manualmente.
+    protected $fillable = ['nome', 'telefone', 'email', 'motivo_contato', 'mensagem'];
 
 }
