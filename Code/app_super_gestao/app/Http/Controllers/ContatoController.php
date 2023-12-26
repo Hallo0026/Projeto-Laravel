6b50bde0310d83cee62bfccd013ceac19ebb278a
Hallo0026 <vinicius_santossouza@outlook.com>
2023-12-26 18:51:53 -0300

    Customizando feedbacks de erro

--- Code/app_super_gestao/app/Http/Controllers/ContatoController.php
+++ Code/app_super_gestao/app/Http/Controllers/ContatoController.php
@@ -18,20 +18,29 @@ class ContatoController extends Controller
 
     public function salvar(Request $request) {
 
-        // realizar a validação dos dados do formulário recebidos no request
-        $request->validate([
-            'nome' => 'required|min:3|max:40',
+        $regras = [
+            'nome' => 'required|min:3|max:40|unique:site_contatos', // Unique por questões de teste.
             'telefone' => 'required',
             'email' => 'email',
             'motivo_contato' => 'required',
             'mensagem' => 'required|max:2000'
-        ]);
+        ];
+
+        $feedback = [
+            'nome.min'                  => 'O campo nome precisa ter no mínimo 3 caracteres',
+            'nome.min'                  => 'O campo nome pode ter no máximo 40 caracteres',
+            'nome.unique'               => 'O nome informado já está em uso',
+            'email.email'               => 'O campo email precisa ser preenchido com um endereço de email válido',
+            'mensagem.max'              => 'O campo mensagem pode ter no máximo 2000 caracteres',
+            // Também é possível customizar o erro somente da validação sem definir um input específico.
+            'required' => 'O campo :attribute deve ser preenchido'
+        ];
+
+        $request->validate($regras, $feedback); // realizar a validação dos dados do formulário recebidos no request
 
         SiteContato::create($request->all());
 
         return redirect()->route('site.index');
-
-        dd($request);
 
     }
 }
--- Code/app_super_gestao/resources/views/site/layouts/_components/form_contato.blade.php
+++ Code/app_super_gestao/resources/views/site/layouts/_components/form_contato.blade.php
@@ -5,11 +5,19 @@
     @csrf <!-- Token necessário pelo Laravel para enviar requisições POST -->
 
     <input type="text" name="nome" value="{{ old('nome') }}" placeholder="Nome" class="{{ $classe }}">
+    @if($errors->has('nome'))
+        {{ $errors->first() }}
+    @endif
     <br>
+
     <input type="text" name="telefone" value="{{ old('telefone') }}" placeholder="Telefone" class="{{ $classe }}">
+    {{ $errors->has('telefone') ? $errors->first('telefone') : '' }}
     <br>
+
     <input type="text" name="email" value="{{ old('email') }}" placeholder="E-mail" class="{{ $classe }}">
+    {{ $errors->has('email') ? $errors->first('email') : '' }}
     <br>
+
     <select name="motivo_contato" class="{{ $classe }}">
 
         <option value="">Qual o motivo do contato?</option>
@@ -21,15 +29,27 @@
         @endforeach
 
     </select>
+    {{ $errors->has('motivo_contato') ? $errors->first('motivo_contato') : '' }}
     <br>
+
     <textarea name="mensagem" class="{{ $classe }}">{{ (old('mensagem') != '') ? old('mensagem') : 'Preencha aqui a sua mensagem' }}
     </textarea>
+    {{ $errors->has('mensagem') ? $errors->first('mensagem') : '' }}
     <br>
+
     <button type="submit" class="{{ $classe }}">ENVIAR</button>
 </form>
 
-<div style="position: absolute; top: 0px; left: 0px; width: 100%; background-color: darkred;">
-    <pre>
-        {{ print_r($errors) }}
-    </pre>
-</div>
+<!--
+@if($errors->any())
+
+    <div style="position: absolute; top: 0px; left: 0px; width: 100%; text-align: center; background-color: darkred; color: white">
+
+        @foreach($errors->all() as $erro)
+            {{ $erro }} <br>
+        @endforeach
+
+    </div>
+
+@endif
+-->
