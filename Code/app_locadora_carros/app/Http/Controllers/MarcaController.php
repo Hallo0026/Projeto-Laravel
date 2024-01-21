505e132278e651a449b3f2b5b6285a47479f1193
Hallo0026 <vinicius_santossouza@outlook.com>
2024-01-21 19:58:37 -0300

    Atualizando registros via PUT/PATCH

--- Code/app_locadora_carros/app/Http/Controllers/MarcaController.php
+++ Code/app_locadora_carros/app/Http/Controllers/MarcaController.php
@@ -17,14 +17,6 @@ class MarcaController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
@@ -43,19 +35,12 @@ class MarcaController extends Controller
     }
 
     /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Marca $marca)
-    {
-        //
-    }
-
-    /**
      * Update the specified resource in storage.
      */
     public function update(Request $request, Marca $marca)
     {
-        //
+        $marca->update($request->all());
+        return $marca;
     }
 
     /**
