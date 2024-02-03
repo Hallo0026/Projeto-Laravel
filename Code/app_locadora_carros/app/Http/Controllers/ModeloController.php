e0c77d595ae9e4dc5c5e09fa39050d383e0227f0
Hallo0026 <vinicius_santossouza@outlook.com>
2024-02-03 16:01:02 -0300

    Adicionado endpoints do resource Modelo

--- Code/app_locadora_carros/app/Http/Controllers/ModeloController.php
+++ Code/app_locadora_carros/app/Http/Controllers/ModeloController.php
@@ -4,62 +4,134 @@ namespace App\Http\Controllers;
 
 use App\Models\Modelo;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class ModeloController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+
+    public $modelo;
+
+    public function __construct(Modelo $modelo) {
+        $this->modelo = $modelo;
+    }
+
+
     public function index()
     {
-        //
+        return response()->json($this->modelo->all(), 200);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
+        // Por padrão o validate retorna para a página anterior, para alterar isso, o cliente definir o header da requisão Accept como application/json, desta forma será retornado um json com os erros.
+        $request->validate($this->modelo->rules());
+
+        $imagem = $request->file('imagem');
+
+        /*
+         * 1º parametro: pasta onde será salvo a imagem
+         * 2º parametro: disco onde será salvo a imagem
+         */
+        $imagem_urn = $imagem->store('imagens/modelos', 'public');
+
+        //dd($request->file('imagem'));
+
+        $modelo = $this->modelo->create([
+            'marca_id' => $request->marca_id,
+            'nome' => $request->nome,
+            'imagem' => $imagem_urn,
+            'numero_portas' => $request->numero_portas,
+            'lugares' => $request->lugares,
+            'air_bag' => $request->air_bag,
+            'abs' => $request->abs,
+        ]);
+
+        return response()->json($modelo, 201);
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Modelo $modelo)
+
+    public function show($id)
     {
-        //
+        $modelo = $this->modelo->find($id);
+
+        if($modelo === null) {
+          //return ['erro' => 'Recurso pesquisado nao existe.'];
+          return response()->json(['erro' => 'Recurso pesquisado nao existe.'], 404);
+        }
+
+        return $modelo;
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Modelo $modelo)
+
+    public function update(Request $request, $id)
     {
-        //
+        $modelo = $this->modelo->find($id);
+
+        if($modelo === null) {
+            return response()->json(['erro' => 'Impossivel realizar a atualizacao. O recurso solicitado nao existe.'], 404);
+        }
+
+
+        if($request->method() === 'PATCH') {
+            return ['teste' => 'Verbo PATCH'];
+
+            $regrasDinamicas = array();
+
+            foreach($modelo->rules() as $input => $regra) {
+                if(array_key_exists($input, $request->all())) {
+                    $regrasDinamicas[$input] = $regra;
+                }
+            }
+
+            $request->validate($modelo->rules());
+
+        } else {
+            $request->validate($modelo->rules());
+        }
+
+        // Remove o arquivo antigo caso um novo arquivo tenha sido enviado no request
+        if($request->file('imagem')) {
+            Storage::disk('public')->delete($modelo->imagem);
+        }
+
+        $imagem = $request->file('imagem');
+
+        /*
+         * 1º parametro: pasta onde será salvo a imagem
+         * 2º parametro: disco onde será salvo a imagem
+         */
+        $imagem_urn = $imagem->store('imagens/modelos', 'public');
+
+        //dd($request->file('imagem'));
+
+        $modelo->update([
+            'marca_id' => $request->marca_id,
+            'nome' => $request->nome,
+            'imagem' => $imagem_urn,
+            'numero_portas' => $request->numero_portas,
+            'lugares' => $request->lugares,
+            'air_bag' => $request->air_bag,
+            'abs' => $request->abs,
+        ]);
+
+        return response()->json($modelo, 200);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Modelo $modelo)
+
+    public function destroy($id)
     {
-        //
+        $modelo = $this->modelo->find($id);
+
+        if($modelo === null) {
+          //return ['erro' => 'Impossivel realizar a exclusao. O recurso solicitado nao existe.'];
+            return response()->json(['erro' => 'Impossivel realizar a exclusao. O recurso solicitado nao existe.'], 404);
+        }
+
+        Storage::disk('public')->delete($modelo->imagem);
+
+        $modelo->delete();
+        return ['msg' => 'O modelo foi removido com sucesso.'];
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Modelo $modelo)
-    {
-        //
-    }
 }
